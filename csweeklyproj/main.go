bedfd8bce34a571befe6079ff7d12b05355bb3c7
Mooshiiee <mooshcor@outlook.com>
2024-08-17 16:31:26 -0400

    added Turso db

--- csweeklyproj/main.go
+++ csweeklyproj/main.go
@@ -4,9 +4,15 @@ package main
 //(as opposed to a library)
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 type problem struct {
@@ -18,84 +24,52 @@ type problem struct {
 	IsProject   bool   ` json:"isproject" `
 }
 
-var problems = []problem{
-	{
-		ID:          "1",
-		Text:        "Implement a function to reverse a string.",
-		Hint:        "Think about string indexing.",
-		Constraints: "Input string length <= 1000.",
-		Solution:    "Use slicing to reverse the string.",
-		IsProject:   false,
-	},
-	{
-		ID:          "2",
-		Text:        "Find the maximum value in an array of integers.",
-		Hint:        "Iterate through the array and keep track of the maximum value found.",
-		Constraints: "Array length <= 10000.",
-		Solution:    "Use a loop to compare each element with the current maximum.",
-		IsProject:   false,
-	},
-	{
-		ID:          "3",
-		Text:        "Design a simple web server in Go.",
-		Hint:        "Look into the net/http package.",
-		Constraints: "Must handle at least basic GET requests.",
-		Solution:    "Use http.HandleFunc and http.ListenAndServe.",
-		IsProject:   true,
-	},
-}
-
-// getProblems returns a list of all the problems as JSON
-// gin.context is important to gin. Mainly, t contains the request and serialize/deserialize the JSON
-func getProblems(c *gin.Context) {
-	//Context.IndentedJSON serializes the struct into JSON and adds it to the response
-	//also returning HTTP_200 via http package and 'StatusOK', and then the data to be serialized
-	c.IndentedJSON(http.StatusOK, problems)
-}
-
-// adds a new problem object from JSON POST data
-// c is a pointer to an instance of 'gin.Context' (not a copy of it)
-func postProblems(c *gin.Context) {
-	var newProblem problem
-	//BindJSON 'binds' the retrieved JSON body to newProblem (deserializing)
-	//will return 400 if error occurs
-	//the "&" passes a pointer to BindJSON, which is required
-	if err := c.BindJSON(&newProblem); err != nil { //nil, kinda like 2-0, 'two - nil'
-		return
-	}
-	//Add the new problem to the problems array
-	problems = append(problems, newProblem)
-	//return HTTP_201 along with the instance the user just created.
-	c.IndentedJSON(http.StatusCreated, newProblem)
-}
-
-func getProblemByID(c *gin.Context) {
-	requestID := c.Param("id")
-
-	//loop over list of items to get the desired one
-	for _, problem := range problems {
-		if problem.ID == requestID {
-			c.IndentedJSON(http.StatusOK, problem)
-			return
-		}
-	}
-	//if nothing
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "problem not found"})
-}
-
 func getIndexPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
 
+func returnENV() {
+
+}
+
+// does not take in anything, returns db and err
+func initDB() (*sql.DB, error) {
+	//initialize env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	url := os.Getenv("TURSO_DATABASE_URL")
+	if url == "" { //check if ENV is working correctly
+		log.Fatal("TURSO_DATABASE_URL environment variable is not set")
+	}
+
+	//initialize turso db
+	db, err := sql.Open("libsql", url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to open db  %s: %s", url, err)
+		os.Exit(1)
+		//"1" means error, "0" means all good
+	}
+	return db, err
+}
+
 func main() {
+
+	db, err := initDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ensure the db closes on exit
+	defer db.Close()
+
 	//initialize Gin Router
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 
 	router.GET("/", getIndexPage)
-	router.GET("/problems", getProblems)
-	router.POST("/problems", postProblems)
-	router.GET("/problems/:id", getProblemByID)
+	router.GET("/router")
 
 	//attach router to http.Server and start it aswell
 	router.Run("localhost:8080")
